73486ccffe47d0fb713f1171f5e783802a696755
Ashutoshanand Sinha <ashutoshanandsinha@Ashutoshanands-MacBook-Pro.local>
2024-11-22 15:41:25 -0800

    optimize model

--- src/model.py
+++ src/model.py
@@ -4,20 +4,49 @@ import torch.nn as nn
 class MNISTNet(nn.Module):
     def __init__(self):
         super(MNISTNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(8)
+
+        # First conv block
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(2)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(16)
+        self.dropout1 = nn.Dropout(0.25)
+
+        # Second conv block
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(2)
-        self.fc = nn.Linear(16 * 7 * 7, 10)
+        self.dropout2 = nn.Dropout(0.25)
+
+        # Third conv block for better feature extraction
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.relu3 = nn.ReLU()
+        self.dropout3 = nn.Dropout(0.25)
+
+        # Fully connected layer
+        self.fc = nn.Linear(32 * 7 * 7, 10)
 
     def forward(self, x):
-        x = self.pool1(self.relu1(self.bn1(self.conv1(x))))
-        x = self.pool2(self.relu2(self.bn2(self.conv2(x))))
-        x = x.view(-1, 16 * 7 * 7)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+        x = self.dropout1(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+        x = self.dropout2(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu3(x)
+        x = self.dropout3(x)
+
+        x = x.view(-1, 32 * 7 * 7)
         x = self.fc(x)
         return x
 
--- src/train.py
+++ src/train.py
@@ -11,23 +11,25 @@ from model import MNISTNet
 def train():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        [transforms.ToTensor(), transforms.RandomRotation(10), transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)), transforms.Normalize((0.1307,), (0.3081,))]
     )
     train_dataset = datasets.MNIST(
         root="./data", train=True, transform=transform, download=True
     )
     train_loader = torch.utils.data.DataLoader(
-        dataset=train_dataset, batch_size=128, shuffle=True, num_workers=2
+        dataset=train_dataset, batch_size=64, shuffle=True, num_workers=2, pin_memory=True
     )
     model = MNISTNet().to(device)
     print(f"Total parameters: {model.count_parameters()}")
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
     scheduler = optim.lr_scheduler.OneCycleLR(
         optimizer,
-        max_lr=0.001,
+        max_lr=0.1,
         epochs=1,
-        steps_per_epoch=len(train_loader)
+        steps_per_epoch=len(train_loader),
+        pct_start=0.2,
+        anneal_strategy='cos'
     )
     total_step = len(train_loader)
     model.train()
